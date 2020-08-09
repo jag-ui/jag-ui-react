3f6cc47cdfe20ab8125700c9543b7d4f03f8c1e0
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-08-08 22:48:32 -0700

    cu

--- packages/components/src/Card.js
+++ packages/components/src/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@jag-ui-react/core";
 
+// TODO: we can delete this
 export const Card = React.forwardRef((props, ref) => (
   <Box ref={ref} variant="primary" boxShadow="default" {...props} __themeKey="cards" />
 ));
