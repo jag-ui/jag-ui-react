e26ad5ab43ff845162052b99f4813a1043daaa57
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-08-28 22:35:46 -0700

    removed: bulk packages

--- packages/components/src/Card.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Box } from "@jag-ui-react/box";
-
-// TODO: we can delete this
-export const Card = React.forwardRef((props, ref) => (
-  <Box ref={ref} variant="primary" boxShadow="default" {...props} __themeKey="cards" />
-));
