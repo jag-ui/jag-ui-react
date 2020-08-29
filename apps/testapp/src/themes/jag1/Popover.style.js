9a33b1cf1249de716baf8cc1a641985a2b7d7de5
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-08-28 17:02:43 -0700

    updated: popover

--- apps/testapp/src/themes/jag1/Popover.style.js
+++ apps/testapp/src/themes/jag1/Popover.style.js
@@ -1,38 +1,4 @@
-export const popover = {
-  baseStyle: {
-    boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
-    borderRadius: "5px",
-    backgroundColor: "white",
-    // padding: "10px",
-    '&[data-popper-placement^="top"] > .popover-arrow': {
-      bottom: "5px",
-      ":after": { boxShadow: "1px 1px 1px rgba(0, 0, 0, 0.1)" },
-    },
-    '&[data-popper-placement^="bottom"] > .popover-arrow': {
-      top: "-5px",
-      ":after": { boxShadow: "-1px -1px 1px rgba(0, 0, 0, 0.1)" },
-    },
-    '&[data-popper-placement^="left"] > .popover-arrow': {
-      right: "5px",
-      ":after": { boxShadow: "1px -1px 1px rgba(0, 0, 0, 0.1)" },
-    },
-    '&[data-popper-placement^="right"] > .popover-arrow': {
-      left: "-5px",
-      ":after": { boxShadow: "-1px 1px 1px rgba(0, 0, 0, 0.1)" },
-    },
-  },
-  variants: {
-    solid: {
-      variantStyle: {},
-      // variantColors: { ...alertColors },
-    },
-  },
-  defaultProps: {
-    variant: "solid",
-  },
-};
-
-export const arrow = {
+const arrow = {
   baseStyle: {
     position: "absolute",
     width: "0",
@@ -48,6 +14,44 @@ export const arrow = {
       height: "10px",
       transform: "rotate(45deg)",
     },
+  },
+  variants: {
+    solid: {
+      variantStyle: {},
+      // variantColors: { ...alertColors },
+    },
+  },
+  defaultProps: {
+    variant: "solid",
+  },
+};
+
+const popoverPosArrowStyle = {
+  '&[data-popper-placement^="top"] > .popover-arrow': {
+    bottom: "5px",
+    ":after": { boxShadow: "1px 1px 1px rgba(0, 0, 0, 0.1)" },
+  },
+  '&[data-popper-placement^="bottom"] > .popover-arrow': {
+    top: "-5px",
+    ":after": { boxShadow: "-1px -1px 1px rgba(0, 0, 0, 0.1)" },
+  },
+  '&[data-popper-placement^="left"] > .popover-arrow': {
+    right: "5px",
+    ":after": { boxShadow: "1px -1px 1px rgba(0, 0, 0, 0.1)" },
+  },
+  '&[data-popper-placement^="right"] > .popover-arrow': {
+    left: "-5px",
+    ":after": { boxShadow: "-1px 1px 1px rgba(0, 0, 0, 0.1)" },
+  },
+};
+
+const popover = {
+  baseStyle: {
+    boxShadow: "0 0 5px rgba(0, 0, 0, 0.3)",
+    borderRadius: "5px",
+    backgroundColor: "white",
+    // padding: "10px",
+    ...popoverPosArrowStyle,
   },
   variants: {
     solid: {
