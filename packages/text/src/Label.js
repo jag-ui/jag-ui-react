a8b0c36d9a93c0f0b48d8ff29aed883b5f47de7a
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-09-08 01:51:51 -0700

    cu

--- packages/text/src/Label.js
+++ packages/text/src/Label.js
@@ -2,5 +2,5 @@ import { Box } from "@jag-ui-react/box";
 import React from "react";
 
 export const Label = React.forwardRef((props, ref) => (
-  <Box ref={ref} as="label" __themeKey="Label.root" __css={{ display: "flex" }} {...props} />
+  <Box ref={ref} as="label" __themeKey="Label.root" __css={{ display: "flex", alignItems: "center" }} {...props} />
 ));
