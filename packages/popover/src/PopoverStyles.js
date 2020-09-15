f52190c43dd7e7f1d7716df5c56939e8afdc7abb
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-09-15 02:17:02 -0700

    u: popover

--- packages/popover/src/PopoverStyles.js
+++ packages/popover/src/PopoverStyles.js
@@ -1,3 +1,5 @@
+const arrowSize = "6px";
+
 export const rootStyle = {
   position: "absolute",
   display: "block",
@@ -8,15 +10,15 @@ export const rootStyle = {
   backgroundClip: "padding-box",
 
   // TOP:
-  "&[data-popper-placement^=top]": { marginBottom: "8px" },
+  "&[data-popper-placement^=top]": { marginBottom: arrowSize },
   "&[data-popper-placement^=top] .popover-arrow": {
-    bottom: "calc((8px + 1px) * -1)",
+    bottom: `calc((${arrowSize} + 1px) * -1)`,
   },
   "&[data-popper-placement=top-start] .popover-arrow": { left: "0" },
   "&[data-popper-placement=top-end] .popover-arrow": { right: "0" },
 
   "&[data-popper-placement^=top] .popover-arrow:after, &[data-popper-placement^=top] .popover-arrow:before": {
-    borderWidth: "8px 8px 0",
+    borderWidth: `${arrowSize} ${arrowSize} 0`,
   },
   "&[data-popper-placement^=top] .popover-arrow:before": {
     bottom: "0",
@@ -28,12 +30,12 @@ export const rootStyle = {
   },
 
   // BOTTOM:
-  "&[data-popper-placement^=bottom]": { marginTop: "8px" },
+  "&[data-popper-placement^=bottom]": { marginTop: arrowSize },
   "&[data-popper-placement^=bottom] .popover-arrow": {
-    top: "calc((8px + 1px) * -1)",
+    top: `calc((${arrowSize} + 1px) * -1)`,
   },
   "&[data-popper-placement^=bottom] .popover-arrow:after, &[data-popper-placement^=bottom] .popover-arrow:before": {
-    borderWidth: "0 8px 8px",
+    borderWidth: `0 ${arrowSize} ${arrowSize}`,
   },
   "&[data-popper-placement^=bottom] .popover-arrow:before": {
     top: "0",
@@ -46,14 +48,14 @@ export const rootStyle = {
 
   // LEFT
 
-  "&[data-popper-placement^=left]": { marginRight: "8px" },
+  "&[data-popper-placement^=left]": { marginRight: arrowSize },
   "&[data-popper-placement^=left] .popover-arrow": {
-    right: "calc((8px + 1px) * -1)",
+    right: `calc((${arrowSize} + 1px) * -1)`,
     width: "10px",
     height: "10px",
   },
   "&[data-popper-placement^=left] .popover-arrow:after, &[data-popper-placement^=left] .popover-arrow:before": {
-    borderWidth: "8px 0 8px 8px",
+    borderWidth: `${arrowSize} 0 ${arrowSize} ${arrowSize}`,
   },
   "&[data-popper-placement^=left] .popover-arrow:before": {
     right: "0",
@@ -66,12 +68,12 @@ export const rootStyle = {
 
   // RIGHT
 
-  "&[data-popper-placement^=right]": { marginLeft: "8px" },
+  "&[data-popper-placement^=right]": { marginLeft: arrowSize },
   "&[data-popper-placement^=right] .popover-arrow": {
-    left: "calc((8px + 1px) * -1)",
+    left: `calc((${arrowSize} + 1px) * -1)`,
   },
   "&[data-popper-placement^=right] .popover-arrow:after, &[data-popper-placement^=right] .popover-arrow:before": {
-    borderWidth: "8px 8px 8px 0",
+    borderWidth: `${arrowSize} ${arrowSize} ${arrowSize} 0`,
   },
   "&[data-popper-placement^=right] .popover-arrow:before": {
     left: "0",
@@ -84,7 +86,7 @@ export const rootStyle = {
 
   // COMMON
   "&[data-popper-placement=left-end] .popover-arrow, &[data-popper-placement=right-end] .popover-arrow": {
-    margin: "calc((8px + 1px) * -1) 0",
+    margin: `calc((${arrowSize} + 1px) * -1) 0`,
   },
   "&[data-popper-placement=left] .popover-arrow, &[data-popper-placement=right] .popover-arrow": {
     margin: "calc((4px + 1px) * -1) 0",
@@ -94,8 +96,8 @@ export const rootStyle = {
 export const arrowStyle = {
   position: "absolute",
   display: "block",
-  width: "8px",
-  height: "8px",
+  width: arrowSize,
+  height: arrowSize,
   ":after,:before": {
     position: "absolute",
     display: "block",
